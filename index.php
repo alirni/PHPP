5de3ce6f4394a0d2bffefab60d486802b5d4319e
alirni <aliramezani19.1@gmail.com>
2018-01-17 22:52:28 +0330

    save cookie and login to site

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
+  // if(!isset($_COOKIE['userLogin'])) {
+  //   header('Location: login.php');
+  //   // setcookie('userLogin', $username, time() + 3600);
+  // }
+
   $con = mysqli_connect("localhost", "root", "110110@", "phpproject");
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $qSelect = "SELECT * FROM user WHERE username=". "'". $username. "'". " AND password=$password";
+
+  $result = mysqli_query($con, $qSelect);
+
+  if(!$result || mysqli_num_rows($result) <= 0) {
+    header('Location: login.php');
+  }
+
+  if(!isset($_COOKIE['userLogin'])) {
+    // header('Location: login.php');
+    setcookie('userLogin', $username, time() + 3600);
+  }
+  else {
+    // setcookie('userLogin', $username, time() + 3600);
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,38 +39,30 @@
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
 
   <!-- Custom Fonts -->
-  <!-- <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"> -->
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
   <!-- <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet"> -->
 
   <!-- Custom CSS -->
   <link href="css/main.css" rel="stylesheet" />
-
 </head>
 
-<body id="page-top">
-  <!-- Header -->
-  <header class="masthead d-flex">
-    <?php
-      $username = $_POST['username'];
-      $password = $_POST['password'];
-
-      $qSelect = "SELECT * FROM user WHERE username=". "'". $username. "'". " AND password=$password";
-
-      $result = mysqli_query($con, $qSelect);
-    ?>
-    <div class="container text-center my-auto">
-      <?php
-        if(!$result || mysqli_num_rows($result) <= 0) {
-          echo "result: ". $result;
-          echo "errooooor";
-        }
-        else {
-          echo "true";
-        }
-      ?>
+<body id="page-top background">
+  <!-- Nav -->
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">WebSiteName</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Page 1</a></li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+      </ul>
     </div>
-  </header>
+  </nav>
+
 
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
@@ -54,9 +70,6 @@
 
   <!-- Plugin JavaScript -->
   <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for this template -->
-  <script src="js/stylish-portfolio.min.js"></script>
 
 </body>
 
